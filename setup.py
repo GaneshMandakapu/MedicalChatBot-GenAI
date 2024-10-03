e65848319d8ed680e91e10c205f039ff113579a0
GaneshManda <mandakapuganesh@gmail.com>
2024-10-03 10:35:30 +0200

    folder structure added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical ChATbOT Gen AI',
+    version= '0.0.0',
+    author= 'Ganesh Balaraju',
+    author_email= 'ganeshbalarajude@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
